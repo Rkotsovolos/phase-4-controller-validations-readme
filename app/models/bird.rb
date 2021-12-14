a5139fe1235fd4cfd76471a854841b4f62e36ca6
Ryan Kotsovolos <rkotsovolos@gmail.com>
2021-12-14 10:11:42 -0800

    commit tree 3502d7600fbdb94cb47a5d269eb05fd3a602d0f4

--- app/models/bird.rb
+++ app/models/bird.rb
@@ -1,2 +1,16 @@
 class Bird < ApplicationRecord
+
+    def create
+        # create! exceptions will be handled by the rescue_from ActiveRecord::RecordInvalid code
+        bird = Bird.create!(bird_params)
+        render json: bird, status: :created
+      end
+      
+      def update
+        bird = find_bird
+        # update! exceptions will be handled by the rescue_from ActiveRecord::RecordInvalid code
+        bird.update!(bird_params)
+        render json: bird
+      end
+
 end
